167bd89f5eaa83d9aaa7dbf20e148407c9d375e1
Vikulov Andrey <vikulov.prog@gmail.com>
2022-04-07 00:31:51 +0400

    final course

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
-od_dict = {'00': 'night', '01': 'night', '02': 'ночь',
+tod_dict = {'00': 'night', '01': 'Ahri_Popstar_30599', '02': 'ночь',
             '03': 'раннее утро', '04': 'раннее утро', '05': 'раннее утро',
             '06': 'утро', '07': 'утро', '08': 'утро',
             '09': 'первая половина дня', '10': 'первая половина дня', '11': 'первая половина дня',
             '12': 'обед', '13': 'обед', '14': 'обед',
             '15': 'после обеда', '16': 'после обеда', '17': 'после обеда',
-            '18': 'вечер', '19': 'вечер', '20': 'вечер',
-            '21': 'поздний вечер', '22': 'поздний вечер', '23': 'поздний вечер'}
+            '18': 'Ahri_Popstar_30599', '19': 'Ahri_Popstar_30599', '20': 'Ahri_Popstar_30599',
+            '21': 'Ahri_Popstar_30599', '22': 'поздний вечер', '23': 'поздний вечер'}
+
+
+
+
+def tick():
+    watch.after(200, tick)
+    watch['text'] = time.strftime("%H:%M:%S")
+    watch_hours = time.strftime("%H")
+    for val, key in tod_dict.items():
+        if int(watch_hours) == int(val):
+            print(val, key)
+            photo = Image.open(f"{Path.cwd()}/" + tod_dict[val] + ".png")
+            img = photo.resize((width, height))
+            image = ImageTk.PhotoImage(img)
+            l_image = ttk.Label(top_frame, image=image)
+            l_image.place(relwidth=0.3, relheight=0.2)
+
+            # img = Image.open(f"{Path.cwd()}/{tod_dict[val]}.png")
+            # print(f"{Path.cwd()}/{tod_dict[val]}.png")
+
+watch = Label(top_frame, font="Arial 25")
+watch.pack(expand=1)
+watch.after_idle(tick)
+
+
+{'coord': {'lon': 37.6156, 'lat': 55.7522}, 'weather': [{'id': 803, 'main': 'Clouds', 'description': 'облачно с прояснениями', 'icon': '04n'}], 'base': 'stations', 'main': {'temp': -1.71, 'feels_like': -4.41, 'temp_min': -1.97, 'temp_max': -0.9, 'pressure': 1004, 'humidity': 88, 'sea_level': 1004, 'grnd_level': 986}, 'visibility': 10000, 'wind': {'speed': 1.97, 'deg': 226, 'gust': 4.44}, 'clouds': {'all': 80}, 'dt': 1649269538, 'sys': {'type': 2, 'id': 47754, 'country': 'RU', 'sunrise': 1649213274, 'sunset': 1649261745}, 'timezone': 10800, 'id': 524901, 'name': 'Москва', 'cod': 200}
--- main_gis.py
+++ main_gis.py
@@ -1,23 +1,14 @@
 # pip install ttkthemes
 # pip install requests
 # pip install translate
+import time
 from tkinter import ttk
 from tkinter import *
 from ttkthemes import ThemedTk
 from tkinter import messagebox
 from translate import Translator
+import requests
 from pathlib import Path
-import time
-from PIL import Image, ImageTk
-
-tod_dict = {'00': 'night', '01': 'Ahri_Popstar_30599', '02': 'ночь',
-            '03': 'раннее утро', '04': 'раннее утро', '05': 'раннее утро',
-            '06': 'утро', '07': 'утро', '08': 'утро',
-            '09': 'первая половина дня', '10': 'первая половина дня', '11': 'первая половина дня',
-            '12': 'обед', '13': 'обед', '14': 'обед',
-            '15': 'после обеда', '16': 'после обеда', '17': 'после обеда',
-            '18': 'вечер', '19': 'вечер', '20': 'вечер',
-            '21': 'поздний вечер', '22': 'поздний вечер', '23': 'поздний вечер'}
 
 API_KEY = '7a8289c6fe2c48775c4d2464078220be'
 API_URL = 'https://api.openweathermap.org/data/2.5/weather'
@@ -25,49 +16,65 @@ API_URL = 'https://api.openweathermap.org/data/2.5/weather'
 width = 80
 height = 80
 
-def get_weather():
-    pass
+translator = Translator(from_lang='Russian', to_lang='English')  # язык для перевода  на английский
 
 
-def tick():
-    watch.after(200, tick)
-    watch['text'] = time.strftime("%H:%M:%S")
-    watch_hours = time.strftime("%H")
-    for val, key in tod_dict.items():
-        if int(watch_hours) == int(val):
-            print(val, key)
-            photo = Image.open(f"{Path.cwd()}/{tod_dict[val]}.png")
-            img = photo.resize((width, height))
-            image = ImageTk.PhotoImage(img)
-            l_image = ttk.Label(top_frame, image=f'{image}')
-            l_image.place(relwidth=0.3, relheight=1)
-            # img = Image.open(f"{Path.cwd()}/{tod_dict[val]}.png")
-            # print(f"{Path.cwd()}/{tod_dict[val]}.png")
+def print_weather(weather):
+    try:
+        city = weather['name']
+        country = weather['sys']['country']
+        temp = weather['main']['temp']
+        press = weather['main']['pressure']
+        humidity = weather['main']['humidity']
+        wind = weather['wind']['speed']
+        desc = weather['weather'][0]['description']
+        sunrise = time.strftime("%H:%M:%S", time.localtime(weather['sys']['sunrise']))
+        sunset = time.strftime("%H:%M:%S", time.localtime(weather['sys']['sunset']))
+        return f"Местоположение: {city}, {country} \nТемпература: {temp} °С \nАтм. давление: {press} гПа \nВлажность: {humidity}% \nСкорость ветра: {wind} м/с \nПогодные условия {desc} \nВосход: {sunrise} \nЗакат {sunset}"
+    except:
+        return "Ошибка получения данных..."
 
 
-root = ThemedTk(theme="ark")
-root.geometry("500x500")
+def get_weather(event=''):
+    if not entry.get():
+        messagebox.showwarning("Warning", 'Введите запрос в формате город, код города')
+    else:
+        params = {
+            "appid": API_KEY,
+            "q": translator.translate(entry.get()),
+            "units": "metric",
+            "lang": "ru"
+        }
+        r = requests.get(API_URL, params=params)
+        weather = r.json()
+        label['text'] = print_weather(weather)
+
+
+root = ThemedTk(theme="arc")
+root.geometry("400x500")
 root.title("Погода")
 root.resizable(False, False)
 root.iconbitmap(f'{Path.cwd()}/cloud.ico')
+
+s = ttk.Style()
+s.configure("TLabel", padding=5, font="Arial 11")
 top_frame = ttk.Frame(root)
-top_frame.place(relx=0.5, rely=0.1, relwidth=0.7, relheight=0.2, anchor='n')
-watch = ttk.Label(top_frame, font="Arial 25")
-watch.pack(expand=1)
-watch.after_idle(tick)
+top_frame.place(relx=0.5, rely=0.1, relwidth=0.9, relheight=0.1, anchor='n')
 
-
-bottom_frame = ttk.Frame(root)
-bottom_frame.place(relx=0.5, rely=0.3, relwidth=0.9, relheight=0.1, anchor='n')
-
-entry = ttk.Entry(bottom_frame)
+entry = ttk.Entry(top_frame)
 entry.place(relwidth=0.7, relheight=1)
 
-button = ttk.Button(bottom_frame, text="Запрос погоды", command=get_weather)
-button.place(relwidth=0.3, relheight=1)
+button = ttk.Button(top_frame, text="Запрос погоды", command=get_weather)
+button.place(relx=0.7, relwidth=0.3, relheight=1)
 
-translator = Translator(from_lang='Russian', to_lang='English')  # язык для перевода  на английский
 # translation = translator.translate("Москва")
 
+lower_frame = ttk.Frame(root)
+lower_frame.place(relx=0.5, rely=0.25, relwidth=0.9, relheight=0.6, anchor='n')
+
+label = ttk.Label(lower_frame, anchor="nw")
+label.place(relheight=1, relwidth=1)
+
+entry.bind("<Return>", get_weather)
 
 root.mainloop()
